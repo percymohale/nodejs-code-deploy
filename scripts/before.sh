23750bd11c7a6471835c6fb96ab8abc634eac7ec
Percy <percymohale9@gmail.com>
2022-11-15 17:13:25 +0200

    suer

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # Install node.js and PM2 globally
-sudo yum update
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install --lts
-node -e "console.log('Running Node.js ' + process.version)"
-sudo yum install npm  -y
+sudo apt-get update
+sudo apt-get install curl
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
+sudo apt-get install nodejs
+sudo apt-get install npm  -y
 sudo npm install pm2 -g
-sudo rm -rf /home/ec2-user/my-app1
+sudo rm -rf /home/ubuntu/my-app1
 
 
 # Install Wordpress
-# sudo yum update -y
+# sudo apt-get update -y
 # sudo amazon-linux-extras install -y php7.2
 # sudo yum install -y httpd
 # sudo systemctl start httpd
 # sudo systemctl enable httpd
 # sudo usermod -a -G apache ec2-user
+
+
+#deploy dev
