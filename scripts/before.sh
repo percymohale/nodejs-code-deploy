d8197808388d8c856f7b37effd88d3b07a411c41
Percy <percymohale9@gmail.com>
2022-11-15 19:55:27 +0200

    suer

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Install node.js and PM2 globally
 sudo yum update -y
-nvm install 16
+
 sudo yum install -y gcc gcc-c++ make openssl-devel git
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash - 
 sudo yum install -y nodejs
+sudo nvm install 16
 node -v
 npm -v
 sudo npm install pm2@latest -g
-sudo pm2 startup
+/usr/local/bin/pm2 pm2 startup
 sudo rm -rf /home/ec2-user/my-app1
 
 
