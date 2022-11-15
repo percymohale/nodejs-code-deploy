3f7162d4927294915bdc82f3139c83c4c6921581
Percy <percymohale9@gmail.com>
2022-11-15 15:46:56 +0200

    suer

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # Install node.js and PM2 globally
 sudo yum update
-sudo yum install nodejs-legacy -y
+sudo yum install nodejs npm --enablerepo=epel
 sudo yum install npm  -y
 sudo npm install pm2 -g
 sudo rm -rf /home/ec2-user/my-app1
+
+
+# Install Wordpress
+sudo yum update -y
+sudo amazon-linux-extras install -y php7.2
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo usermod -a -G apache ec2-user
