01a2c26775e3cb76f99a59a10b09e1c626fb34c8
Percy <percymohale9@gmail.com>
2022-11-15 19:00:52 +0200

    suer

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Install node.js and PM2 globally
 sudo yum update
+sudo yum install nodejs
+node -v
 sudo npm install pm2 -g
 sudo rm -rf /home/ec2-user/my-app1
-pm2 stop all
-pm2 start /home/ec2-user/my-app1/app/index.js
+
 
 # Install Wordpress
 # sudo apt-get update -y
